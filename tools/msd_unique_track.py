5e7bfef7b75f385e6df1e7fad492fa89cc32e9e0
Michael Vötter <mikevo-uibk@famv.net>
2019-03-15 13:27:33 +0100

    Add matching for msd and billboard

--- tools/msd_unique_track.py
+++ tools/msd_unique_track.py
@@ -5,6 +5,24 @@ import pandas as pd
 
 
 def main():
+    match_msd_billboard()
+
+
+def match_msd_billboard():
+    msd = read_msd_tracks()
+    billboard = read_billboard_tracks()
+
+    combined = pd.merge(
+        msd,
+        billboard,
+        how='inner',
+        left_on=['artist', 'title'],
+        right_on=['artist', 'title'])
+    with open('msd_bb_matches.csv', 'w') as out_file:
+        combined.to_csv(out_file)
+
+
+def msd_track_duplicates():
     msd = read_msd_tracks()
     unique_file_count = len(set(msd['track_file']))
     unique_id_count = len(set(msd['track_id']))
@@ -27,7 +45,13 @@ def read_msd_tracks():
         file_path,
         sep='<SEP>',
         header=None,
-        names=['track_file', 'track_id', 'track_artist', 'track_title'])
+        names=['track_file', 'track_id', 'artist', 'title'])
+
+
+def read_billboard_tracks():
+    file_path = '/storage/nas3/datasets/music/billboard_mp3/billboard_1954-2018_summary.csv'
+
+    return pd.read_csv(file_path)
 
 
 if __name__ == '__main__':
