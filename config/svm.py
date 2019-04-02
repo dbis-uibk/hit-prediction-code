b8f891a91156ac1d5b0556490f8ad803aa1334f3
Michael Vötter <mikevo-uibk@famv.net>
2019-04-02 11:21:48 +0200

    Shuffle data in svm

--- config/svm.py
+++ config/svm.py
@@ -1,11 +1,14 @@
 import dbispipeline.result_handlers as result_handlers
 from dbispipeline.evaluators import GridEvaluator
 
-from sklearn.svm import SVR
+from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR
 
 from dataloaders import MsdBbLoader
 
+
+cv = KFold(n_splits=5, shuffle=True, random_state=42)
 
 dataloader = MsdBbLoader(
         hits_file_path='/storage/nas3/datasets/music/billboard/msd_bb_matches.csv',  # noqa E501
@@ -26,7 +29,7 @@ evaluator = GridEvaluator(
     },
     grid_parameters={
         'verbose': 3,
-        'cv': 5,
+        'cv': cv,
         'refit': False,
         'scoring': 'neg_mean_squared_error',
     },
