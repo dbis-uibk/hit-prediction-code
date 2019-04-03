184867dbdaffd5e135561c7b316438dc94a6b82d
Michael Vötter <mikevo-uibk@famv.net>
2019-04-03 14:00:41 +0200

    add more specific feature sets

--- common.py
+++ common.py
@@ -9,6 +9,12 @@ def feature_columns(columns, feature_select):
 
     if feature == 'hl':
         regex_filter = highlevel_regex()
+    elif feature == 'genre':
+        regex_filter = genre_regex()
+    elif feature == 'mood':
+        regex_filter = mood_regex()
+    elif feature == 'voice':
+        regex_filter = voice_regex()
     else:
         regex_filter = feature
 
@@ -17,8 +23,20 @@ def feature_columns(columns, feature_select):
 
 def filter_features(columns, regex_filter):
     regex = re.compile(regex_filter)
-    return list(filter(regex.search, columns))
+    return list(filter(regex.match, columns))
 
 
 def highlevel_regex():
     return r'highlevel\.\w+\.all\.\w+'
+
+
+def genre_regex():
+    return r'highlevel\.genre_tzanetakis\.all\.\w+'
+
+
+def mood_regex():
+    return r'highlevel\.mood_\w+\.all\.[a-z]+$'
+
+
+def voice_regex():
+    return r'(highlevel\.voice_instrumental\.all\.\w+|highlevel\.gender\.all\.\w+)'
