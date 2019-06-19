71451a2c0756ac2044a6c138843554603f0b1ffa
Michael Vötter <mikevo-uibk@famv.net>
2019-06-19 12:58:34 +0200

    use artist as a deep feature (combine again after one-hot)

--- config/wide_and_deep_all.py
+++ config/wide_and_deep_all.py
@@ -20,7 +20,7 @@ dataloader = MsdBbLoader(
     non_hits_per_hit=1,
     features=[
         *common.all_list(),
-        ('artist', 'wide'),
+        ('artist', 'deep'),
     ],
     label='peak',
     nan_value=150,
