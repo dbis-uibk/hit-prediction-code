3e315932eb576e10cfd9d3bd11a22ac03ac6c172
Michael Vötter <mikevo-uibk@famv.net>
2019-07-04 11:27:49 +0200

    update artist info crawling

--- tools/artist_info.py
+++ tools/artist_info.py
@@ -1,3 +1,5 @@
+from urllib import parse
+
 import click
 
 import matplotlib.pyplot as plt
@@ -26,27 +28,30 @@ def cli(path):
 @cli.command()
 def artists():
     hits = read_hits()
-    hit_artists = pd.read_csv('/storage/nas3/datasets/wikipedia/wikidata/artist_dbpedia_wikidata_hits.csv') 
+    hit_artists = pd.read_csv(
+        '/storage/nas3/datasets/wikipedia/wikidata/artist_dbpedia_wikidata_hits.csv'
+    )
     artist_id = read_msd_unique_artists()
     artist_id = artist_id.dropna(subset=['mb_artist_id'])
-    num_of_hits = len(hits)
+    # num_of_hits = len(hits)
 
-    hits_mbid = join(hits, artist_id, on=['artist'])
-    hits_wd = join(hits, hit_artists, on=['artist'])
-    hit_artists = join(hit_artists, artist_id, on=['artist'])
-    hits = join(hits, hit_artists, on=['artist'])
+    # hits_mbid = join(hits, artist_id, on=['artist'])
+    # hits_wd = join(hits, hit_artists, on=['artist'])
+    # hit_artists = join(hit_artists, artist_id, on=['artist'])
+    # hits = join(hits, hit_artists, on=['artist'])
 
-    print(num_of_hits, len(hits_mbid), len(hits_wd), len(hits))
+    # print(num_of_hits, len(hits_mbid), len(hits_wd), len(hits))
 
     hits = hits.groupby([
         'artist',
     ]).size().sort_values(ascending=False).reset_index(name='counts')
 
-    hits = hits[hits['artist'].str.contains('[0-9]', regex=True)]
+    no_info = set(hits['artist']) - set(hit_artists['artist'])
 
+    print(len(no_info), no_info)
     # print(hits)
-    hits.plot()
-    plt.show()
+    # hits.plot()
+    # plt.show()
 
 
 def artist_preprocess(name):
@@ -59,68 +64,77 @@ def artist_preprocess(name):
 
 @cli.command()
 def dbpedia():
+    dest_file = RESULT_PATH + '/artist_dbpedia_wikidata.csv'
     mapping = []
 
-    artists = read_non_hits().drop_duplicates(subset=['artist'])
+    artists = read_hits()
+    artists = artists.append(read_non_hits(), sort=False)
+    artists.drop_duplicates(subset=['artist'], inplace=True)
 
     num_of_artists = len(artists)
     for i, artist in enumerate(artists['artist'], 1):
         entry = {}
         entry['artist'] = artist
         try:
-            result = get_artist_from_dbpedia(entry['artist'])
-        except SPARQLExceptions.QueryBadFormed:
+            result, _ = get_artist_from_dbpedia(entry['artist'])
+        except SPARQLExceptions.QueryBadFormed as ex:
+            print(ex)
             continue
 
         if result:
-            print(result)
-
             entry['artist_dbpedia_uri'] = result['item']['value']
             entry['artist_wikidata_uri'] = result['same']['value']
             mapping.append(entry)
-        print('Track:', i, '/', num_of_artists)
+
+        print('Track:', i, '/', num_of_artists, '    found:', bool(result),
+              '    ', artist)
 
         if i % 100 == 0:
-            pd.DataFrame(mapping).to_csv(RESULT_PATH + '/artist_dbpedia_wikidata.csv')
+            pd.DataFrame(mapping).to_csv(dest_file)
 
-    pd.DataFrame(mapping).to_csv(RESULT_PATH + '/artist_dbpedia_wikidata.csv')
+    pd.DataFrame(mapping).to_csv(dest_file)
 
 
 def get_artist_from_dbpedia(artist):
+    # artist = parse.quote(artist, safe=' ')
+    print(artist)
+
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setQuery("""
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX purl: <http://purl.org/linguistics/gold/>
         SELECT DISTINCT ?item ?same
         WHERE
         {
-            { ?item purl:hypernym dbr:Singer . }
+            { ?item rdf:type schema:MusicGroup . }
             UNION
-            { ?item purl:hypernym dbr:Band . }
+            { ?item rdf:type dbo:MusicalArtist . }
+            UNION
+            { ?item rdf:type dbo:Band . }
             ?item rdfs:label ?label .
             FILTER (lang(?label) = 'en')
-            FILTER (str(?label) = '""" + artist + """')
+            FILTER (str(?label) = "%s")
             ?item owl:sameAs ?same .
             FILTER (regex(?same, 'wikidata.org'))
         } LIMIT 2
-    """)
+    """ % artist)
     sparql.setReturnFormat(JSON)
     results = sparql.query()
+    info = results.info()
 
-    print(results.info())
     results = results.convert()["results"]["bindings"]
-    print(len(results))
 
     if len(results) == 1:
-        return results[0]
+        return results[0], info
     else:
-        return None
+        return None, info
 
 
 @cli.command()
 def wikidata():
     data = {}
-    non_hit_artists = pd.read_csv('/storage/nas3/datasets/wikipedia/wikidata/artist_dbpedia_wikidata_non_hits.csv')
+    non_hit_artists = pd.read_csv(
+        '/storage/nas3/datasets/wikipedia/wikidata/artist_dbpedia_wikidata_non_hits.csv'
+    )
 
     for artist in non_hit_artists['artist_wikidata_uri']:
         artist_data = get_entity_dict_from_api(artist.rsplit('/', 1)[-1])
