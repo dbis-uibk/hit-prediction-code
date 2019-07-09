a1ee9d83113148264a01aa49909878c8de9b9444
Michael Vötter <mikevo-uibk@famv.net>
2019-07-09 09:03:03 +0200

    Update billboard crawler

--- tools/charts.py
+++ tools/charts.py
@@ -17,7 +17,16 @@ def cli(path):
 
 @cli.command()
 def billboard_100():
-    charts = billboard.ChartData('hot-100')
+    load_billboard_charts('hot-100')
+
+
+@cli.command()
+def billboard_200():
+    load_billboard_charts('billboard-200')
+
+
+def load_billboard_charts(name):
+    charts = billboard.ChartData(name)
     end_date = charts.date
     known_songs = set()
 
@@ -27,22 +36,26 @@ def billboard_100():
         for hit in charts:
             if str(hit) not in known_songs:
                 known_songs.add(str(hit))
-                entries.append(hit.__dict__)
+                entry = {
+                    **hit.__dict__,
+                    'latest-date': charts.date,
+                }
+                entries.append(entry)
 
         start_date = charts.date
         print(start_date, 'week', count)
-        charts = billboard.ChartData('hot-100', charts.previousDate)
+        charts = billboard.ChartData(name, charts.previousDate)
 
         if (count % 52 == 1) and (start_date != end_date):
             bb_hits = pd.DataFrame(entries)
-            bb_hits.to_csv(RESULT_PATH + '/billboard_100_' + str(start_date) +
-                           '_' + str(end_date) + '.csv')
+            bb_hits.to_csv(RESULT_PATH + '/billboard_' + name + '_' +
+                           str(start_date) + '_' + str(end_date) + '.csv')
 
         count += 1
 
     bb_hits = pd.DataFrame(entries)
-    bb_hits.to_csv(RESULT_PATH + '/billboard_100_' + str(start_date) + '_' +
-                   str(end_date) + '.csv')
+    bb_hits.to_csv(RESULT_PATH + '/billboard_' + name + '_' + str(start_date) +
+                   '_' + str(end_date) + '.csv')
 
 
 if __name__ == '__main__':
