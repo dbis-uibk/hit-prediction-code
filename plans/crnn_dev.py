f5e487ac8d7d692abe866d289b690f597ced21b6
Michael Vötter <mikevo-uibk@famv.net>
2020-02-06 14:25:30 +0100

    Update crnn and crnn_dev plans

--- plans/crnn_dev.py
+++ plans/crnn_dev.py
@@ -23,7 +23,7 @@ evaluator = GridEvaluator(
         'model__batch_size': [64],
         'model__epochs': [1, 2, 4],
         'model__num_dense_layer': [0, 1, 2],
-        'model__loss': ['mean_squared_error'],
+        'model__loss': ['mean_absolute_error', 'mean_squared_error'],
     },
     grid_parameters=evaluations.grid_parameters(),
 )
