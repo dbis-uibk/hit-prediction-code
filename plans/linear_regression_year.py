0002094d5e65d3a5649b8639b844cdd38c4e4a81
Michael Vötter <mikevo-uibk@famv.net>
2020-02-17 10:54:13 +0100

    Add a linreg year plan.

---
+++ plans/linear_regression_year.py
@@ -0,0 +1,31 @@
+from dbispipeline.evaluators import GridEvaluator
+import dbispipeline.result_handlers as result_handlers
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+dataloader = EssentiaLoader(
+    dataset_path='data/processed/msd_bb_balanced_essentia.pickle',
+    features=[
+        ('year', 'wide'),
+    ],
+    label='peak',
+    nan_value=150,
+)
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('linreg', LinearRegression()),
+])
+
+evaluator = GridEvaluator(
+    parameters={},
+    grid_parameters=evaluations.grid_parameters(),
+)
+
+result_handlers = [
+    result_handlers.print_gridsearch_results,
+]
