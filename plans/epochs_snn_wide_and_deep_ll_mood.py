e954af9a3de7bfb5cbb4d9c742b82685c2d5a249
Michael Vötter <mikevo-uibk@famv.net>
2020-04-09 12:54:07 +0200

    Add epoch evaluator plan for w&d ll mood

---
+++ plans/epochs_snn_wide_and_deep_ll_mood.py
@@ -0,0 +1,44 @@
+"""SNN Wide and deep model evaluation plan using mood and lowlevel features."""
+from dbispipeline.evaluators import CvEpochEvaluator
+import dbispipeline.result_handlers
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.wide_and_deep import WideAndDeep
+
+dataloader = EssentiaLoader(
+    dataset_path='data/processed/msd_bb_balanced_essentia.pickle',
+    features=[
+        *common.all_ll_mood_list(),
+    ],
+    label='peak',
+    nan_value=150,
+)
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model',
+     WideAndDeep(
+         epochs=500,
+         features=dataloader.feature_indices,
+         batch_normalization=False,
+         deep_activation='selu',
+         dense_activation='selu',
+         output_activation='elu',
+         num_dense_layer=7,
+         dropout_rate=0.1,
+     )),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(),
+    scoring_step_size=5,
+)
+
+result_handlers = [
+    dbispipeline.result_handlers.print_gridsearch_results,
+]
