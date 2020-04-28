4ca3ac06aa4d4167f39a5e11d0995ac0413d6cd5
Michael Vötter <mikevo-uibk@famv.net>
2020-04-28 11:42:23 +0200

    add wide and deep epoch evaluation plans

---
+++ plans/epochs_wide_and_deep_ll.py
@@ -0,0 +1,44 @@
+"""Wide and deep model epoch evaluation plan using lowlevel features."""
+from dbispipeline.evaluators import CvEpochEvaluator
+import dbispipeline.result_handlers
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.wide_and_deep import WideAndDeep
+
+dataloader = EssentiaLoader(
+    dataset_path='data/processed/msd_bb_balanced_essentia.pickle',
+    features=[
+        *common.ll_list(),
+    ],
+    label='peak',
+    nan_value=150,
+)
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model',
+     WideAndDeep(
+         epochs=1000,
+         features=dataloader.feature_indices,
+         batch_normalization=False,
+         deep_activation='elu',
+         dense_activation='elu',
+         output_activation='elu',
+         num_dense_layer=7,
+         dropout_rate=0.1,
+     )),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(),
+    scoring_step_size=10,
+)
+
+result_handlers = [
+    dbispipeline.result_handlers.print_gridsearch_results,
+]
