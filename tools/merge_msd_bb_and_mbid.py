210e59587f2f08d99df4b02ab861a9dc3ae5f124
Michael Vötter <mikevo-uibk@famv.net>
2020-07-22 14:20:32 +0200

    Add logging for merge tools

--- tools/merge_msd_bb_and_mbid.py
+++ tools/merge_msd_bb_and_mbid.py
@@ -1,12 +1,14 @@
 """Merges the ismir2019 dataset with mbids."""
+from logzero import logger
 import pandas as pd
 
+logger.info('Merge msd_bb_matches with mbid')
 msd_mbid_map = pd.read_csv(
     'data/hit_song_prediction_ismir2020/raw/msd-mbid-2016-01-results-ab.csv',
     names=['msd_id', 'mbid', 'title', 'artist'],
 )
 
-# merge mateches
+logger.info('Load msd_bb_matches.csv')
 matches = pd.read_csv(
     'data/hit_song_prediction_ismir2019/msd_bb_matches.csv',
     index_col=0,
@@ -17,19 +19,21 @@ matches_mbid = matches.merge(
     on=['msd_id'],
     suffixes=('_msd_bb', '_msd_mbid'),
 )
+
+logger.info('Store msd_bb_mbid_matches.csv')
 matches_mbid.to_csv(
     'data/hit_song_prediction_ismir2020/interim/msd_bb_mbid_matches.csv')
 
-# merge non-mateches
+logger.info('Load msd_bb_non_matches.csv')
 non_matches = pd.read_csv(
     'data/hit_song_prediction_ismir2019/msd_bb_non_matches.csv',
     index_col=0,
 )
-
 non_matches_mbid = non_matches.merge(
     msd_mbid_map,
     on=['msd_id'],
     suffixes=('_msd_bb', '_msd_mbid'),
 )
+logger.info('Store msd_bb_mbid_non_matches.csv')
 non_matches_mbid.to_csv(
     'data/hit_song_prediction_ismir2020/interim/msd_bb_mbid_non_matches.csv')
--- tools/merge_msd_bb_mbid_and_ab.py
+++ tools/merge_msd_bb_mbid_and_ab.py
@@ -1,6 +1,7 @@
 """loads acoustic brainz features for mbids in msd_bb_mbid dataset."""
 import os.path
 
+from logzero import logger
 import pandas as pd
 
 from hit_prediction_code.dataloaders import acousticbrainz
@@ -12,6 +13,7 @@ def _load_features(name, feature):
     songs = pd.read_csv(os.path.join(path_prefix, name + '.csv'))
     mbids = list(songs['mbid'])
 
+    logger.info('Combine %s features for %s' % (feature, name))
     acousticbrainz.load_ab_features_as_df(mbids, 'll').to_parquet(
         os.path.join(
             path_prefix,
@@ -20,6 +22,7 @@ def _load_features(name, feature):
 
 
 # combine all features
+logger.info('Combine acousticbrainz features for msd_bb_mbid')
 for filename in ['msd_bb_mbid_matches', 'msd_bb_mbid_non_matches']:
     for feature_type in ['ll', 'hl']:
         _load_features(filename, feature_type)
