632c11c8fea4a4b744846004b5c88c22053959ea
Michael Vötter <mikevo-uibk@famv.net>
2020-09-29 14:17:59 +0200

    update n-gram plan

--- plans/cleaned_melspect_unique_ngram_peak.py
+++ plans/cleaned_melspect_unique_ngram_peak.py
@@ -10,6 +10,7 @@ from sklearn.pipeline import Pipeline
 
 from hit_prediction_code.dataloaders import MelSpectLoader
 import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.transformers.mel_spect import FloatListToSentence
 from hit_prediction_code.transformers.mel_spect import MelSpectScaler
 from hit_prediction_code.transformers.mel_spect import ZOrderTransformer
 
@@ -28,17 +29,17 @@ dataloader = MelSpectLoader(
 pipeline = Pipeline([
     ('scale', MelSpectScaler()),
     ('z-order', ZOrderTransformer()),
-    ('vectorizer', CountVectorizer(analyzer='word')),
+    ('sentence', FloatListToSentence(round_decimals=3)),
+    ('vector', CountVectorizer(analyzer='word')),
     ('tf-idf', TfidfTransformer()),
     ('model', SGDRegressor()),
 ])
 
 evaluator = GridEvaluator(
     parameters={
-        'vectorizer__max_df': (0.5, 0.75, 1.0),
-        # 'vectorizer__max_features': (None, 5000, 10000, 50000),
-        'vectorizer__ngram_range': (
-            (1, 1), (1, 2), (1, 3), (1, 4), (1, 8)),  # unigrams or bigrams
+        # 'vector__max_df': (0.5, 0.75, 1.0),
+        # 'vector__max_features': (None, 5000, 10000, 50000),
+        'vector__ngram_range': ((1, 1), (1, 4)),  # unigrams or bigrams
         # 'tf-idf__use_idf': (True, False),
         # 'tf-idf__norm': ('l1', 'l2'),
         'model__max_iter': (20,),
