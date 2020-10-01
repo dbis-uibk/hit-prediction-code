4c9f716808b79508ff831dd9ff7ab7ae86e422d0
Michael Vötter <mikevo-uibk@famv.net>
2020-10-01 14:27:26 +0200

    Add mel-spect feature extraction tool

---
+++ tools/ismir2020/extract_features.py
@@ -0,0 +1,40 @@
+"""Extracts features from melspects."""
+import os.path
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+from hit_prediction_code.dataloaders import MelSpectLoader
+from hit_prediction_code.transformers.mel_spect import FloatListToSentence
+from hit_prediction_code.transformers.mel_spect import MelSpectScaler
+from hit_prediction_code.transformers.mel_spect import ZOrderTransformer
+
+PATH_PREFIX = 'data/hit_song_prediction_ismir2020/processed'
+
+dataloader = MelSpectLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_bb_mbid_cleaned_matches_melspect_unique.pickle.xz',
+    ),
+    features='librosa_melspectrogram',
+    label='peakPos',
+    nan_value=150,
+)
+
+pipeline = Pipeline([
+    ('scale', MelSpectScaler()),
+    ('z-order', ZOrderTransformer()),
+    ('sentence', FloatListToSentence(round_decimals=3)),
+    ('vector', CountVectorizer(analyzer='word')),
+    ('tf-idf', TfidfTransformer()),
+])
+
+tf_idf = pipeline.fit_transform(dataloader.load()[0])
+tf_idf.to_pickle(
+    os.path.join(
+        PATH_PREFIX,
+        'analytics_tfidf_msd_bb_mbid_cleaned_matches_melspect_unique.pickle.xz',  # noqa:E501
+    ),
+    'xz',
+)
