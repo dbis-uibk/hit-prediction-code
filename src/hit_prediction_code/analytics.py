c9b6c325b2457695f351650d098e2487eea4812e
Michael Vötter <mikevo-uibk@famv.net>
2021-02-02 16:29:10 +0100

    Add restult_analytics notebook

--- src/hit_prediction_code/analytics.py
+++ src/hit_prediction_code/analytics.py
@@ -1,5 +1,23 @@
 """Analytics code used to process results."""
+import os.path
+from typing import Dict, List
+
 import pandas as pd
+
+
+def _splits_from_outcome(outcome: pd.Series) -> List[str]:
+
+    def check_number(v):
+        try:
+            int(v)
+            return True
+        except ValueError:
+            return False
+
+    if len(outcome) > 0:
+        return [k for k in outcome.iloc[0].keys() if check_number(k)]
+    else:
+        return []
 
 
 def aggregate_splits_per_epoch(outcome, aggregation_function):
@@ -13,15 +31,7 @@ def aggregate_splits_per_epoch(outcome, aggregation_function):
     Returns:
         A dataframe with aggregated results for all splits.
     """
-
-    def check_number(v):
-        try:
-            int(v)
-            return True
-        except ValueError:
-            return False
-
-    splits = [k for k in outcome.iloc[0].keys() if check_number(k)]
+    splits = _splits_from_outcome(outcome=outcome)
 
     result = []
     for row in outcome:
@@ -38,7 +48,7 @@ def aggregate_splits_per_epoch(outcome, aggregation_function):
     return pd.DataFrame(result)
 
 
-def aggregate_epochs(outcome, aggregation_function):
+def aggregate_epochs(outcome, aggregation_function) -> pd.DataFrame:
     """
     Aggregates epochs.
 
@@ -58,3 +68,66 @@ def aggregate_epochs(outcome, aggregation_function):
             aggregation_function, axis=1)
 
     return result
+
+
+def _extract_cv_epoch_evaluator_outcome_entry(
+    outcome: pd.Series,
+    key='mean',
+) -> pd.Series:
+    """Extracts an entry from the cv epoch evaluator results.
+
+    The entry is either the mean, the standard deviation or a split.
+
+    Args:
+        results (pd.Series): the results dataframe containing the outcome.
+        key (str, optional): the key name of the entry that should be extacted.
+            Defaults to 'mean'.
+
+    Returns:
+        pd.Series: the extracted entry.
+    """
+    return outcome.apply(lambda v: pd.DataFrame(v[key]))
+
+
+def _extract_splits_form_cv_epoch_evaluator_outcome(
+        outcome: pd.Series) -> Dict[str, pd.Series]:
+    splits = sorted(_splits_from_outcome(outcome=outcome))
+
+    extracted = {}
+
+    for split in splits:
+        split_key = 'split-' + split
+        extracted[split_key] = _extract_cv_epoch_evaluator_outcome_entry(
+            outcome,
+            key=split,
+        )
+
+    return extracted
+
+
+def add_cv_epoch_evaluator_outcome_to_df(df: pd.DataFrame) -> None:
+    """Adds all entries from cv epoch evaluator outcome as dataframe columns.
+
+    Args:
+        df (pd.DataFrame): the dataframe that is modified.
+    """
+    for key in ['mean', 'stdev']:
+        df[key] = _extract_cv_epoch_evaluator_outcome_entry(
+            outcome=df['outcome'],
+            key=key,
+        )
+
+    for key, result in _extract_splits_form_cv_epoch_evaluator_outcome(
+            outcome=df['outcome']).items():
+        df[key] = result
+
+
+def add_approach_to_df(df: pd.DataFrame) -> None:
+    """Add an approach column to the dataframe.
+
+    Args:
+        df (pd.DataFrame): the dataframe that is modified.
+    """
+    df['approach'] = df['sourcefile'].apply(
+        lambda v: os.path.splitext(os.path.basename(v))[0].replace(
+            'unique_', '').replace('wide_and_deep', 'wd'))
