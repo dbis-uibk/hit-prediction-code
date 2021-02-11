ba1e3eef37c5ebf25769a0e9d12ea2f746b0b29d
Michael Vötter <mikevo-uibk@famv.net>
2021-02-11 08:54:22 +0100

    Add ordinal classifier impl

---
+++ plans/ordinal/cleaned_ab_unique_ordinal_et_all_weeks.py
@@ -0,0 +1,59 @@
+"""Wide and deep model evaluation plan using all features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import ExtraTreeClassifier
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.ordinal import OrdinalClassifier
+from hit_prediction_code.result_handlers import print_results_as_json
+from hit_prediction_code.transformers.label import \
+    convert_array_to_closest_labels
+
+PATH_PREFIX = 'data/hit_song_prediction_ismir2020/processed'
+
+dataloader = EssentiaLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_bb_mbid_cleaned_matches_ab_unique.parquet',
+    ),
+    features=[
+        *common.all_no_year_list(),
+    ],
+    label='weeks',
+    nan_value=common.weeks_non_hit_value(),
+)
+
+labels = common.weeks_labels()
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model',
+     OrdinalClassifier(
+         ExtraTreeClassifier,
+         lambda y: convert_array_to_closest_labels(y, labels=labels),
+     )),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(
+        hit_nonhit_accuracy_score=lambda evaluator, x, y: evaluations.metrics.
+        hit_nonhit_accuracy_score(
+            evaluator,
+            x,
+            y,
+            threshold=0.5,
+        ),
+        categories=labels,
+    ),
+    scoring_step_size=evaluations.scoring_step_size(),
+)
+
+result_handlers = [
+    print_results_as_json,
+]
---
+++ src/hit_prediction_code/models/ordinal.py
@@ -0,0 +1,54 @@
+"""Module containing implementations for ordinal classification."""
+import numpy as np
+from sklearn.base import clone
+
+
+class OrdinalClassifier():
+    """Ordinal classifier implementation.
+
+    Taken from: https://gist.github.com/M46F/c574f688715d5f7e4b65bce4b3ec5fdc
+    """
+
+    def __init__(self, clf, label_mapper):
+        """Initializes the classifier.
+
+        Args:
+            clf: base classifier.
+            label_mapper: function used to map labels.
+        """
+        self.clf = clf
+        self.clfs = {}
+        self.label_mapper = label_mapper
+
+    def fit(self, data, labels):
+        """Fits the classifier."""
+        labels = self.label_mapper(labels)
+        self.unique_class = np.sort(np.unique(labels))
+        if self.unique_class.shape[0] > 2:
+            for i in range(self.unique_class.shape[0] - 1):
+                # for each k - 1 ordinal value we fit a binary classifier
+                binary_y = (labels > self.unique_class[i]).astype(np.uint8)
+                clf = clone(self.clf)
+                clf.fit(data, binary_y)
+                self.clfs[i] = clf
+
+    def predict_proba(self, data):
+        """Predicts label probabilities."""
+        clfs_predict = {k: self.clfs[k].predict_proba(data) for k in self.clfs}
+        predicted = []
+        for i, y in enumerate(self.unique_class):
+            if i == 0:
+                # V1 = 1 - Pr(y > V1)
+                predicted.append(1 - clfs_predict[y][:, 1])
+            elif y in clfs_predict:
+                # Vi = Pr(y > Vi-1) - Pr(y > Vi)
+                predicted.append(clfs_predict[y - 1][:, 1] -
+                                 clfs_predict[y][:, 1])
+            else:
+                # Vk = Pr(y > Vk-1)
+                predicted.append(clfs_predict[y - 1][:, 1])
+        return np.vstack(predicted).T
+
+    def predict(self, data):
+        """Predicts the target label."""
+        return np.argmax(self.predict_proba(data), axis=1)
