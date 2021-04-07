03ddf659844fa371a7d1d84383d354020171ffa4
Michael Vötter <mikevo-uibk@famv.net>
2021-04-07 10:29:31 +0200

    Add a hit/non-hit plan based on peak position.

---
+++ plans/wide_and_deep_performance/cleaned_ab_unique_wide_and_deep_ordinal_one_hot_all_peak_binary.py
@@ -0,0 +1,57 @@
+"""Wide and deep model evaluation plan using all features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.wide_and_deep import WideAndDeepOrdinal
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_ismir2020/processed'
+
+dataloader = EssentiaLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_bb_mbid_cleaned_matches_ab_unique.parquet',
+    ),
+    features=[
+        *common.all_no_year_list(),
+    ],
+    label='peakPos',
+    nan_value=common.peak_pos_non_hit_value(),
+)
+
+hit_nonhit_labels = [100., 101.]
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model',
+     WideAndDeepOrdinal(
+         epochs=common.wide_and_deep_epochs(),
+         features=dataloader.feature_indices,
+         batch_normalization=False,
+         deep_activation='elu',
+         dense_activation='elu',
+         dropout_rate=0.1,
+         predict_strategy='argmax',
+         vectorization_strategy='one_hot',
+         loss='categorical_crossentropy',
+         metrics=['mse', 'categorical_crossentropy'],
+         output_activation='softmax',
+         labels=hit_nonhit_labels,
+     )),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(categories=hit_nonhit_labels),
+    scoring_step_size=evaluations.scoring_step_size(),
+)
+
+result_handlers = [
+    print_results_as_json,
+]
