e1c2ac3ad60d9637dd29987abdf484e9448e5329
Michael Vötter <mikevo-uibk@famv.net>
2021-08-03 15:04:27 +0200

    Add logistic regression plans.

---
+++ plans/wide_and_deep_performance/binary_class_lastfm_ab_unique_logit_all_lfmlc.py
@@ -0,0 +1,42 @@
+"""LogisticRegressionClassifier model evaluation plan using all features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import BinaryClassLoaderWrapper
+from hit_prediction_code.dataloaders import EssentiaLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.linear import LogisticRegressionClassifier
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_lastfm/processed'
+
+dataloader = BinaryClassLoaderWrapper(wrapped_loader=EssentiaLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_lastfm_matches_ab_unique.parquet',
+    ),
+    features=[
+        *common.all_no_year_list(),
+    ],
+    label='lastfm_listener_count',
+    nan_value=0,
+))
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model', LogisticRegressionClassifier()),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.classifier_scoring(),
+    scoring_step_size=1,
+)
+
+result_handlers = [
+    print_results_as_json,
+]
