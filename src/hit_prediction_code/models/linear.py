6eb8baf6a254ed9e895d179375f03aaf09220c39
Michael Vötter <mikevo-uibk@famv.net>
2021-08-04 10:27:51 +0200

    Add linear regression plans

--- src/hit_prediction_code/models/linear.py
+++ src/hit_prediction_code/models/linear.py
@@ -5,6 +5,31 @@ from sklearn import linear_model
 from ..transformers.label import convert_array_to_class_vector
 
 
+class LinearRegression(linear_model.LinearRegression):
+    """Wrapper class for the sklearn linear regression regressor."""
+
+    def __init__(self):
+        """Creates the wrapped regressor."""
+        super().__init__(verbose=True)
+        self.epochs = 1
+
+    def fit(self, data, target, epochs=1):
+        """Wraps the fit of the super class.
+
+        This allows to use this class in an epoch evaluator. Keep in mind that
+        the number of epochs is ignored. Hence, it should only be used with 1
+        epoch.
+
+        Args:
+            data (array-like): the features.
+            target (array-like): the targets.
+            epochs (int, optional): required to fit the api used for
+                evaluation. The value is ignored and it is always trained for 1
+                epoch.
+        """
+        super().fit(data, target)
+
+
 class LogisticRegression(linear_model.LogisticRegression):
     """Wrapper class for the sklearn logistic regression regressor."""
 
