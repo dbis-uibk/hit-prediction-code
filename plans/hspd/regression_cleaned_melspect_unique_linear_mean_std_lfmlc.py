6856af424087795274e72da4bb6c643429872210
Michael Vötter <mikevo-uibk@famv.net>
2021-08-05 14:31:42 +0200

    Add mean, std linreg plans for cleaned dataset

---
+++ plans/hspd/regression_cleaned_melspect_unique_linear_mean_std_lfmlc.py
@@ -0,0 +1,42 @@
+"""LinearRegression model plan using mean and std mel-spect features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import MelSpectMeanStdLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.linear import LinearRegression
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_ismir2020/processed'
+
+dataloader = MelSpectMeanStdLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_bb_mbid_cleaned_matches_melspect_unique.pickle.xz',
+    ),
+    features='librosa_melspectrogram',
+    label='lastfm_listener_count',
+    nan_value=0,
+)
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model', LinearRegression()),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(
+        hit_nonhit_accuracy_score=None,
+        categories=common.lfmlc_labels(),
+    ),
+    scoring_step_size=1,
+)
+
+result_handlers = [
+    print_results_as_json,
+]
