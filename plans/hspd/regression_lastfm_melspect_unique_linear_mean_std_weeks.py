09a3f9b6badff0e7bb84dd06f6447df93cd5bc04
Michael Vötter <mikevo-uibk@famv.net>
2021-08-10 14:01:18 +0200

    Add mel-spect based plans for large datase

---
+++ plans/hspd/regression_lastfm_melspect_unique_linear_mean_std_weeks.py
@@ -0,0 +1,48 @@
+"""LinearRegression model plan using mean and std mel-spect features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import MelSpectMeanStdLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.linear import LinearRegression
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_lastfm/processed'
+
+dataloader = MelSpectMeanStdLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_lastfm_matches_melspect_features_unique.pickle.xz',
+    ),
+    features='librosa_melspectrogram',
+    label='weeks',
+    nan_value=common.weeks_non_hit_value(),
+)
+
+pipeline = Pipeline([
+    ('scale', MinMaxScaler()),
+    ('model', LinearRegression()),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(
+        hit_nonhit_accuracy_score=lambda evaluator, x, y: evaluations.metrics.
+        hit_nonhit_accuracy_score(
+            evaluator,
+            x,
+            y,
+            threshold=0.5,
+        ),
+        categories=common.weeks_labels(),
+    ),
+    scoring_step_size=1,
+)
+
+result_handlers = [
+    print_results_as_json,
+]
