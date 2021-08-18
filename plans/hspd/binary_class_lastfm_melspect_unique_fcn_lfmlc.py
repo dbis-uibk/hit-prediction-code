31500d36e1ca73308ffd1c7879a97cf02734b138
Michael Vötter <mikevo-uibk@famv.net>
2021-08-18 07:48:49 +0200

    add missing fcn plans

---
+++ plans/hspd/binary_class_lastfm_melspect_unique_fcn_lfmlc.py
@@ -0,0 +1,45 @@
+"""FCN plan using mel-spect features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import BinaryClassLoaderWrapper
+from hit_prediction_code.dataloaders import MelSpectLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.cnn import FCN
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_lastfm/processed'
+
+dataloader = BinaryClassLoaderWrapper(wrapped_loader=MelSpectLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_lastfm_matches_melspect_features_unique.pickle',
+    ),
+    features='librosa_melspectrogram',
+    label='lastfm_listener_count',
+    nan_value=0,
+))
+
+pipeline = Pipeline([
+    ('model',
+     FCN(
+         epochs=common.fcn_epochs(),
+         loss='categorical_crossentropy',
+         metrics=['mae', 'mse', 'categorical_crossentropy'],
+         output_activation='softmax',
+         label_output=True,
+     )),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.classifier_scoring(),
+    scoring_step_size=evaluations.fcn_scoring_step_size(),
+)
+
+result_handlers = [
+    print_results_as_json,
+]
---
+++ plans/hspd/regression_cleaned_melspect_unique_fcn_peak.py
@@ -0,0 +1,38 @@
+"""FCN plan using mel-spect features."""
+import os.path
+
+from dbispipeline.evaluators import CvEpochEvaluator
+from sklearn.pipeline import Pipeline
+
+import hit_prediction_code.common as common
+from hit_prediction_code.dataloaders import MelSpectLoader
+import hit_prediction_code.evaluations as evaluations
+from hit_prediction_code.models.cnn import FCN
+from hit_prediction_code.result_handlers import print_results_as_json
+
+PATH_PREFIX = 'data/hit_song_prediction_ismir2020/processed'
+
+dataloader = MelSpectLoader(
+    dataset_path=os.path.join(
+        PATH_PREFIX,
+        'msd_bb_mbid_cleaned_matches_melspect_unique.pickle',
+    ),
+    features='librosa_melspectrogram',
+    label='peakPos',
+    nan_value=common.peak_pos_non_hit_value(),
+)
+
+pipeline = Pipeline([
+    ('model', FCN(epochs=common.fcn_epochs())),
+])
+
+evaluator = CvEpochEvaluator(
+    cv=evaluations.cv(),
+    scoring=evaluations.metrics.scoring(
+        categories=common.peak_position_labels()),
+    scoring_step_size=evaluations.fcn_scoring_step_size(),
+)
+
+result_handlers = [
+    print_results_as_json,
+]
